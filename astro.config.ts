4bf742604dff86f63d59bfa89bf12f69939f895d
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-27 15:39:39 -0700

    feat: add Solid.js support and update TypeScript configuration
    - Added @astrojs/solid-js and solid-js dependencies to package.json
    - Updated tsconfig.json to include jsxImportSource for Solid.js

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jetvolcano.github.io/'
+  site: 'https://jetvolcano.github.io/',
+  integrations: [solidJs()]
 });
