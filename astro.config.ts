4675e75caf5aad6cbc506b0ebf0a1a659a2d549e
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-04-27 20:26:39 -0700

    refactor: remove Solid.js integration and dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jetvolcano.github.io/',
-  integrations: [solidJs()]
 });
