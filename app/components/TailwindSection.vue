c780ba4f438ca935431672d5d80419ae0e5b92e0
JetVolcano <184006209+JetVolcano@users.noreply.github.com>
2025-12-02 20:28:29 -0800

    Update .gitignore to include .vscode folder
    Update CODE_OF_CONDUCT.md list style from asterisk to hypen
    
    Update @nuxt/hints from 1.0.0-alpha.2 to 1.0.0-alpha.3
    
    Update color scheme in main.css
    
    Create a section about what I'm learning W.I.P.
    
    Create TailwindSection.vue which is a special section for the Learning section, when hovered over it rotates

---
+++ app/components/TailwindSection.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex h-15 size-10 hover:rotate-x-30 hover:rotate-z-5 hover:-rotate-y-10 translate-y-1">
+    <NuxtImg
+      src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.d52e9897.svg"
+      class="size-15"
+    />
+    <b class="translate-y-3 translate-x-2 text-3xl">tailwindcss</b>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,5 +3,13 @@
     <div class="flex place-content-center translate-y-2.5">
       <PageHeader />
     </div>
+    <UContainer class="translate-y-10">
+      <div class="outline-2 outline-offset-12 outline-zinc-800 rounded-md">
+        <b class="text-2xl font-bold text-neutral-700 dark:text-slate-200"
+          >What I'm learning</b
+        >
+        <TailwindSection />
+      </div>
+    </UContainer>
   </UApp>
 </template>
